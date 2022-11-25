92b178fe4e719af7b5c6cecbf07076cea5b10dcc
Julian Battaglino <julianbattaglino@gmail.com>
2022-11-24 21:30:39 -0300

    WP api rest posts component

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,6 +5,8 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
 
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
@@ -33,24 +35,29 @@ const Posts = () => {
                         return (
 
                             <Card key={post.id} className="post-card">
-                                <CardMedia className=""
+                                <CardMedia className="post-img"
                                     component="img"
                                     alt={post.title.rendered}
                                     height="140"
                                     image={post._embedded['wp:featuredmedia']['0'].source_url}
 
                                 />
-                                <CardContent>
-                                    <span className=''><b>{post.title.rendered}</b></span>
-                                </CardContent>
-                                <CardActions>
+                                <CardContent className="post-content">
+                                    <a href={post._embedded['wp:term']['0']['0'].link} target="_blank" rel="noreferrer"><h3 className="underline-nice">{"Categoria: " + post._embedded['wp:term']['0']['0'].name}</h3></a>
 
+                                    <h2 className="title"><b>{post.title.rendered}</b></h2>
+                                    <p>Publicado el: {post.formatted_date}</p>
+
+                                </CardContent>
+
+                                <CardActions className="btn-container">
                                     <a href={post.link} target="_blank" rel="noreferrer">
-                                        <Button variant="outlined">
-                                            Ver más
+                                        <Button className="btn-showmore" variant="contained">
+                                            Leer más
                                         </Button>
                                     </a>
                                 </CardActions>
+
                             </Card>
 
                         )
